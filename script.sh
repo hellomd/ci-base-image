f7142b81f988c8fb9305b5282812897f0bcf5bbb
Felix Ribeiro <ribeiro.felix@live.com>
2017-08-31 16:52:59 -0300

    fix ecr repo creation

--- script.sh
+++ script.sh
@@ -16,7 +16,7 @@ aws configure set region $REGION
 eval `aws ecr get-login --no-include-email `            
 docker build -t $CIRCLE_PROJECT_REPONAME:$TAG . 
 docker tag $CIRCLE_PROJECT_REPONAME:$TAG $FULL_IMAGE_NAME:$TAG 
-aws ecr create-repository --repository-name $CIRCLE_PROJECT_REPONAME
+aws ecr create-repository --repository-name $CIRCLE_PROJECT_REPONAME || true
 docker push $FULL_IMAGE_NAME:$TAG 
 
 sed -i Dockerrun.aws.json -e "s/<TAG>/$TAG/" -e "s/<IMAGE_NAME>/$REPOSITORY\/$CIRCLE_PROJECT_REPONAME/" 
